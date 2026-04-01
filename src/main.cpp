9f975edf4efa803ea844374c3db2e89d3a7e5e69
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2026-04-01 20:58:12 +0700

    batman

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #define TRIG 5
 #define ECHO 18
 
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.39701
+
 void setup() {
     Serial.begin(115200);
     pinMode(TRIG, OUTPUT);
@@ -17,13 +20,14 @@ float readDistance() {
     digitalWrite(TRIG, LOW);
 
     long duration = pulseIn(ECHO, HIGH);
-    float distance = duration * 0.034 / 2;
+    float distance = duration * SOUND_SPEED / 2;
 
     return distance;
 }
 
 void loop() {
     float d = readDistance();
-    Serial.println(d);
+	Serial.print("Distance (cm): ");
+	Serial.println(d);
     delay(1000);
 }
